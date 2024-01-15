4d27f3257dd2935c90c73690408f9249dade636c
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2024-01-15 14:06:04 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        pink : "#f87171",
+        hitam : "#0c0a09"
+      }
+    },
+  },
+  plugins: [],
+}
+
