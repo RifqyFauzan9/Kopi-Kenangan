664242409ac71fa17d69f486f60b6662b97e8d38
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2024-01-23 09:11:46 +0700

    a

--- src/js/script.js
+++ src/js/script.js
@@ -30,7 +30,10 @@ window.addEventListener('click', function (a) {
 // darkmode
 const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
-
 darkToggle.addEventListener('click', function () {
-    
-})
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    };
+});
